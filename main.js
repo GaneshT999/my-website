e2c699f38e2cc0328e632b8bb583d866b3b5d0eb
GaneshT999 <107189593+GaneshT999@users.noreply.github.com>
2024-08-15 18:55:16 -0400

    changes

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const typed = new Typed('.multiple-text', {
-    strings: ['Full Stack Developer', 'Data Scientist', 'Cloud Engineer'],
+    strings: ['Full Stack Developer'],
     typeSpeed: 70,
     backSpeed: 70,
     backDelay: 1000,
@@ -16,3 +16,18 @@ ScrollReveal().reveal('.home-content, heading', { origin: 'top' });
 ScrollReveal().reveal('.home-img, .services-container', { origin: 'bottom' });
 ScrollReveal().reveal('.about-heading', { origin: 'left' });
 ScrollReveal().reveal('.about-content', { origin: 'right' });
+
+const menuIcon = document.getElementById('menu-icon');
+    const navbar = document.getElementById('sidebar');
+
+    menuIcon.addEventListener('click', () => {
+        navbar.classList.toggle('active');
+    });
+
+    // Optional: Close the sidebar when a link is clicked
+    const navbarLinks = document.querySelectorAll('.navbar a');
+    navbarLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            navbar.classList.remove('active');
+        });
+    });
