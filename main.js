b9d2297777c719856d2f2b64005ce3260f3bdae5
GaneshT999 <107189593+GaneshT999@users.noreply.github.com>
2024-08-15 22:40:37 -0400

    skills

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const typed = new Typed('.multiple-text', {
 ScrollReveal({
     distance: '80px',
     duration: 2000,
-    delay:200,
+    delay: 200,
 });
 
 ScrollReveal().reveal('.home-content, heading', { origin: 'top' });
@@ -18,16 +18,52 @@ ScrollReveal().reveal('.about-heading', { origin: 'left' });
 ScrollReveal().reveal('.about-content', { origin: 'right' });
 
 const menuIcon = document.getElementById('menu-icon');
-    const navbar = document.getElementById('sidebar');
+const navbar = document.getElementById('sidebar');
+const sidebarOverlay = document.createElement('div');
 
-    menuIcon.addEventListener('click', () => {
-        navbar.classList.toggle('active');
+sidebarOverlay.classList.add('sidebar-overlay');
+document.body.appendChild(sidebarOverlay);
+
+menuIcon.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+    sidebarOverlay.classList.toggle('active');
+});
+
+// Close the sidebar when a link is clicked
+const navbarLinks = document.querySelectorAll('.navbar a');
+navbarLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navbar.classList.remove('active');
+        sidebarOverlay.classList.remove('active');
     });
+});
 
-    // Optional: Close the sidebar when a link is clicked
-    const navbarLinks = document.querySelectorAll('.navbar a');
-    navbarLinks.forEach(link => {
-        link.addEventListener('click', () => {
-            navbar.classList.remove('active');
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll("#sidebar a");
+
+    function removeActiveClasses() {
+        navLinks.forEach(link => link.classList.remove("active"));
+    }
+
+    function addActiveClass(id) {
+        removeActiveClasses();
+        document.querySelector(`#sidebar a[href="#${id}"]`).classList.add("active");
+    }
+
+    function handleScroll() {
+        const sections = document.querySelectorAll("section");
+        let scrollPos = window.scrollY || document.documentElement.scrollTop;
+
+        sections.forEach(section => {
+            if (scrollPos >= section.offsetTop - 50 && scrollPos < section.offsetTop + section.offsetHeight) {
+                const sectionId = section.getAttribute("id");
+                addActiveClass(sectionId);
+            }
         });
-    });
+    }
+
+    window.addEventListener("scroll", handleScroll);
+});
+
+
+
